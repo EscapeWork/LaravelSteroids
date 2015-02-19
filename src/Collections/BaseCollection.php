0e5077b3a8f4770f3da2f71dce3ea856eb960fce
Eduardo Henrique Kasper <ehkasper@gmail.com>
2015-02-19 14:02:19 -0200

    [14:01:23] Base collection added Base Model added Base repo added Sluggable Trait added

---
+++ src/Collections/BaseCollection.php
@@ -0,0 +1,32 @@
+<?php namespace EscapeWork\LaravelSteroids\Collections;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class BaseCollection extends Collection
+{
+
+    /**
+     * Combobox options
+     */
+    protected $comboxBoxOptions = [
+        'empty_option'       => false,
+        'empty_option_label' => 'Selecione',
+        'field'              => 'title',
+    ];
+
+    public function combobox($options = [])
+    {
+        $options = array_merge($this->comboxBoxOptions, $options);
+        $data    = [];
+
+        if ($options['empty_option']) {
+            $data[null] = $options['empty_option_label'];
+        }
+
+        foreach ($this->items as $item) {
+            $data[$item->{$item->getKeyName()}] = $item->$options['field'];
+        }
+
+        return $data;
+    }
+}
---
+++ src/Repositories/Repository.php
@@ -0,0 +1,19 @@
+<?php namespace EscapeWork\LaravelSteroids\Repositories;
+
+abstract class Repository {
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->model, $method), $args);
+    }
+
+    public function __get($key)
+    {
+        return $this->model->getAttribute($key);
+    }
+
+    public function __set($key, $value)
+    {
+        $this->model->setAttribute($key, $value);
+    }
+}
