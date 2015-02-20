4868250391cb2b8b76d23bc15e0416cef59afe53
Eduardo Henrique Kasper <ehkasper@gmail.com>
2015-02-20 11:12:08 -0200

    Sluggable Trait and Repository added as well as a base controller with dispatch commands

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,11 @@
+<?php namespace EscapeWork\LaravelSteroids\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+abstract class Controller extends BaseController {
+
+    use DispatchesCommands, ValidatesRequests;
+
+}
