199ce65a926ce4141a4b8b2bc2df4c52a73191d0
Luis Dalmolin <luis.nh@gmail.com>
2015-09-03 15:27:50 -0300

    Fixed create_links function

--- src/helpers.php
+++ src/helpers.php
@@ -2,10 +2,6 @@
 
 if (! function_exists('create_links')) {
     function create_links($text) {
-        $text = preg_replace("#(^|[\n ])@([^ \"\t\n\r<]*)#ise", "'\\1<a target=\"_blank\" href=\"http://www.twitter.com/\\2\" >@\\2</a>'", $text);
-        $text = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a target=\"_blank\" href=\"\\2\" rel=\"nofollow\">\\2</a>'", $text);
-        $text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a target=\"_blank\" href=\"http://\\2\" rel=\"nofollow\">\\2</a>'", $text);
-
-        return $text;
+        return preg_replace('!(http://[a-z0-9_./?=&-]+)!i', '<a href="$1">$1</a> ', $text." ");
     }
 }
