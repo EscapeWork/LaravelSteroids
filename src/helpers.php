2e16adf79bbcbeb7e0118c190ba6f59306157298
Luis Dalmolin <luis.nh@gmail.com>
2017-02-18 10:28:42 -0200

    Removed some unused things

--- src/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (! function_exists('create_links')) {
-    function create_links($text) {
-        return preg_replace('!(http://[a-z0-9_./?=&-]+)!i', '<a href="$1">$1</a> ', $text." ");
-    }
-}
